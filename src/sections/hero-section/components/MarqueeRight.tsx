c4cdcd5f365c4cb45ab816627e7fd589c6e543c0
AbuPsng <abupsngdscz@gmail.com>
2025-06-20 18:53:06 +0530

    fix the marquee animation for right to left direction for middle one

--- src/sections/hero-section/components/MarqueeRight.tsx
+++ src/sections/hero-section/components/MarqueeRight.tsx
@@ -3,22 +3,22 @@ import Icon from "./Icon";
 const MarqueeRight = ({ iconsArray }: { iconsArray: Array<string> }) => {
 	return (
 		<div className="flex flex-row  overflow-hidden space-x-4 group">
-			<div className="flex space-x-4 w-max animate-marquee-left group-hover:paused">
+			<div className="flex space-x-4 w-max animate-marquee-right group-hover:paused">
 				{iconsArray.map((item, index) => {
 					return <Icon imgUrl={item} key={index} />;
 				})}
 			</div>
-			<div className="flex space-x-4 w-max  animate-marquee-left group-hover:paused">
+			<div className="flex space-x-4 w-max  animate-marquee-right group-hover:paused">
 				{iconsArray.map((item, index) => {
 					return <Icon imgUrl={item} key={index} />;
 				})}
 			</div>
-			<div className="flex space-x-4 w-max  animate-marquee-left group-hover:paused">
+			<div className="flex space-x-4 w-max  animate-marquee-right group-hover:paused">
 				{iconsArray.map((item, index) => {
 					return <Icon imgUrl={item} key={index} />;
 				})}
 			</div>
-			<div className="flex space-x-4 w-max  animate-marquee-left group-hover:paused">
+			<div className="flex space-x-4 w-max  animate-marquee-right group-hover:paused">
 				{iconsArray.map((item, index) => {
 					return <Icon imgUrl={item} key={index} />;
 				})}
